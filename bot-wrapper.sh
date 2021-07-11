5d7b479e4de1c34b24ff0f0c289b9fbfcd32a6fc
Leo Izen <leo.izen@gmail.com>
2021-07-11 13:46:57 -0400

    bot-wrapper.sh: Make friendlier

--- bot-wrapper.sh
+++ bot-wrapper.sh
@@ -1,18 +1,28 @@
 #!/bin/sh
 # bot wrapper
-this_dir="$(dirname "$0")"
+this_dir=$(realpath -- "$(dirname "$0")")
 cd "$this_dir" || exit 2
-this_sh="$(basename "$0")"
-bot_script="${1}.py"
-if ! [ -f "$bot_script" ] ; then
-	printf >&2 'Must provide valid bot name\nUsage: %s BOTNAME\n' "$this_sh"
+this_dirname=$(basename "$this_dir")
+
+if [ -z "$1" ]; then
+    if [ -f "${this_dirname}.py" ]; then
+        main_script=${this_dirname}.py
+    else
+        main_script=main.py
+    fi
+else
+    main_script=${1}.py
+fi
+
+if ! [ -f "$main_script" ] ; then
+	printf >&2 '%s: error: Could not find %s\n' "${0##*/}" "$main_script"
 	exit 1
 fi
 
 relaunch_counter=0
 while [ "$relaunch_counter" -lt 10 ] ; do
     relaunch_counter=$((1 + relaunch_counter))
-    /usr/bin/env python3 "$bot_script"
+    /usr/bin/env python3 "$main_script"
     sleep 5
 done
 exit 2
